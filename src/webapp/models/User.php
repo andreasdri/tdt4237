2de8e614159d4fbb1fc5c447a518a96d90c7da5e
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2015-10-20 15:23:15 +0200

    Add moneyspent and bankcard with getters and setters to User-model

--- src/webapp/models/User.php
+++ src/webapp/models/User.php
@@ -14,7 +14,8 @@ class User
     protected $email   = null;
     protected $bio     = 'Bio is empty.';
     protected $age;
-    protected $bankcard;
+    protected $bankcard = '';
+    protected $moneyspent = 0.0;
     protected $isAdmin = 0;
 
     function __construct($username, $hash, $fullname, $address, $postcode)
@@ -79,7 +80,6 @@ class User
 
     public function setPostcode($postcode) {
         $this->postcode = $postcode;
-
     }
 
     public function isAdmin()
@@ -126,6 +126,29 @@ class User
     public function setIsAdmin($isAdmin)
     {
         $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+    public function setBankcard($bankcard){
+        $this->bankcard = trim($bankcard);
+        return $this;
+    }
+
+    public function getBankcard(){
+        return $this->bankcard;
+    }
+
+    public function getMoneyspent(){
+        return $this->moneyspent;
+    }
+
+    public function setMoneyspent($moneyspent){
+        $this->moneyspent = $moneyspent;
+        return $this;
+    }
+
+    public function spendMoney($money){
+        $this->moneyspent += $money;
         return $this;
     }
 
